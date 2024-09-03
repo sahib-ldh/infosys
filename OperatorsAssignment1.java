4b56db53bfbdf37b3391a96c29d16dd11bd773a8
Sahib Singh <itiglehunt@gmail.com>
2024-09-03 15:36:49 +0530

    programs

---
+++ OperatorsAssignment1.java
@@ -0,0 +1,15 @@
+public class OperatorsAssignment1 {
+    public static void main(String[] args) {
+       
+        double radius = 4;
+        double radius2 = 10;
+        
+        double pi = 3.14;
+        double area = pi* radius*radius;
+        double area2 = pi* radius2*radius2;
+        System.out.println(area + " " + area2);
+
+
+    }
+    
+}
---
+++ OperatorsAssignment2.java
@@ -0,0 +1,17 @@
+public class OperatorsAssignment2 {
+    public static void main(String[] args) {
+       
+        double c;
+        double c2;
+        double f=32;
+        double f2=50;
+        
+        
+        c = ((f-32)/9)*5;
+        c2 = ((f2-32)/9)*5;
+        System.out.println(c + " "+ c2);
+
+
+    }
+    
+}
